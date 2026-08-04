6dd59d418e8ffa6f3bb6780784a5e2bc8498230c
YunHui <805086021@qq.com>
2026-08-04 22:42:56 +0800

    feat: commom button

---
+++ todolist/src/components/commonButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <button
+    class="common-button"
+    :style="{
+      width: props.width ? props.width : '',
+      height: props.height ? props.height : '',
+      backgroundColor: props.bgColor ? props.bgColor : 'var(--primary-color)',
+      color: props.color ? props.color : 'white',
+    }"
+  >
+    <slot name="buttonIcon">{{ props.buttonIcon }} </slot>
+
+    <slot name="buttonName">{{ props.buttonName }}</slot>
+  </button>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  buttonName: string;
+  buttonIcon?: string;
+  width?: string;
+  height?: string;
+  color?: string;
+  bgColor?: string;
+}>();
+</script>
+
+<style scoped>
+.common-button {
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  font-size: 1rem;
+  transition: var(--item-transiton);
+}
+
+.common-button:hover {
+  cursor: pointer;
+  box-shadow: var(--card-shadow);
+}
+</style>
--- todolist/src/components/navBar/navBarIndex.vue
+++ todolist/src/components/navBar/navBarIndex.vue
@@ -1,12 +1,14 @@
 <template>
   <div class="flex flex-col gap-6">
-    <logo />
+    <logo></logo>
 
-    <taskStats />
+    <taskStats></taskStats>
 
-    <priority />
+    <priority></priority>
 
-    <tag />
+    <tag></tag>
+
+    <commonButton :button-icon="'+'" button-name="Add Task"></commonButton>
   </div>
 </template>
 
@@ -17,6 +19,8 @@ const logo = defineAsyncComponent(() => import("./logo.vue"));
 const taskStats = defineAsyncComponent(() => import("./taskStats.vue"));
 const priority = defineAsyncComponent(() => import("./priority.vue"));
 const tag = defineAsyncComponent(() => import("./tag.vue"));
+
+const commonButton = defineAsyncComponent(() => import("../commonButton.vue"));
 </script>
 
 <style></style>
